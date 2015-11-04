72a6a1d2b4706f1dc0347c87394f29b1d315ea6e
Alfred Dobradi <alfreddobradi@gmail.com>
2015-11-04 10:52:25 +0100

    material icons and roboto font

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,8 @@
 <head>
 <meta charset="UTF-8">
     <title>Angular Material + Laravel webapp bootstrap</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=RobotoDraft:300,400,500,700,400italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="{{ elixir('assets/css/vendor.css') }}">
     <link rel="stylesheet" href="{{ elixir('assets/css/app.css') }}">
 </head>
