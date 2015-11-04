119051cb34b9e0b3e1faa511133741a91c344710
Alfred Dobradi <alfreddobradi@gmail.com>
2015-11-04 12:13:33 +0100

    fluff

--- resources/assets/js/controllers/WelcomeController.js
+++ resources/assets/js/controllers/WelcomeController.js
@@ -1,4 +1,4 @@
 var WelcomeController = function($scope) {
     // Not really useful, just to have something dynamic ;3
-    $scope.welcomeHeader = "Welcome y'alls!";
+    $scope.welcomeHeader = "Welcome y'alls! (This is dynamic)";
 };
