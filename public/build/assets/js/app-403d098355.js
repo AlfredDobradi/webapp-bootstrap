bb9a59b320f85777d29259d43d640207bdc61ab1
Alfred Dobradi <alfreddobradi@gmail.com>
2015-11-04 10:45:22 +0100

    ignore bower components, public assets and builds

--- public/build/assets/js/app-403d098355.js
+++
@@ -1,8 +0,0 @@
-var WelcomeController = function($scope) {
-    // Not really useful, just to have something dynamic ;3
-    $scope.welcomeHeader = "Welcome y'alls!";
-};
-var app = angular.module('bootstrap', ['ngMaterial']);
-
-app.controller('WelcomeController', WelcomeController);
-//# sourceMappingURL=app.js.map
