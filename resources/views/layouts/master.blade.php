51599061cbaa4dc6b1fda1e3c77550fa6a9a31a8
Alfred Dobradi <alfreddobradi@gmail.com>
2015-11-04 12:00:51 +0100

    welcome page toolbar, content area

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,19 @@
     <link rel="stylesheet" href="{{ elixir('assets/css/vendor.css') }}">
     <link rel="stylesheet" href="{{ elixir('assets/css/app.css') }}">
 </head>
-<body>
+<body layout="column" layout-fill>
+    <md-toolbar>
+        <div class="md-toolbar-tools">
+            <h2>
+                <span>Angular Material + Laravel bootstrap</span>
+            </h2>
+            <span flex></span>
+            <md-button class="md-fab" href="https://github.com/AlfredDobradi/webapp-bootstrap">
+                <md-icon class="material-icons">favorite</md-icon>
+            </md-button>
+        </div>
+    </md-toolbar>
+
     @yield('content')
 
     <script src="{{ elixir('assets/js/vendor.js') }}"></script>
--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-    <md-content ng-controller="WelcomeController">
+    <md-content ng-controller="WelcomeController" layout-padding layout="column">
         <h2 ng-cloak>@{{ welcomeHeader }}</h2>
         <p>
             This is just a simple welcome page without any purpose.
